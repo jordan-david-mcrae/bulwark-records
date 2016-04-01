9334ba290beb1d590e9a34b913487cf475d56cfc
jordan-david-mcrae <jordanmcrae@hotmail.ca>
2016-03-31 20:11:18 -0400

    Port

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,7 +96,7 @@ module.exports = function(grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 5000,
+        port: 80,
         // Change this to '0.0.0.0' to access the server from outside.
         hostname: '0.0.0.0',
         livereload: 35731
